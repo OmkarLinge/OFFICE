8c77fad2142115df35b3ffb06f64ab6df0ee2073
Omkar Linge <omkarlinge3026@gmail.com>
2026-03-02 17:09:38 +0530

    added Operators.lua

---
+++ Lua_Scripts/Operators.lua
@@ -0,0 +1,29 @@
+-- Operators.lua
+-- Demonstrates arithmetic operators
+
+print("Enter first number:")
+local a = tonumber(io.read())
+
+print("Enter second number:")
+local b = tonumber(io.read())
+
+-- Arithmetic operations
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+-- What Happenes
+-- User enters two numbers → Lua performs calculations.
+-- Operators Used
+-- Operator	Meaning
+--   +	Addition
+--   -	Subtraction
+--   *	Multiplication
+--   /	Division
+--   %	Remainder
+
+-- Learning:
+--       User input
+--       Arithmetic operations
