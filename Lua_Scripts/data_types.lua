e6422d59d3334a7381e108e2dcc69abf97830b5c
Omkar Linge <omkarlinge3026@gmail.com>
2026-03-02 17:12:36 +0530

    added data_types.lua

---
+++ Lua_Scripts/data_types.lua
@@ -0,0 +1,22 @@
+-- data_types.lua
+-- Demonstrates basic Lua data types
+
+local name = "Omkar"     -- string
+local age = 21           -- number
+local isStudent = true   -- boolean
+local location = nil     -- empty value
+
+print("Name:", name)
+print("Age:", age)
+print("Student:", isStudent)
+print("Location:", location)
+
+-- Explanation :- 
+-- Lua mainly uses:
+--                  String → text
+--                  Number → numeric value
+--                  Boolean → true/false
+--                  Nil → no value
+-- Learning:
+-- Variable storage
+-- Type understanding
