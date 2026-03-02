357213525ff6b39e6158d37d5e999040d6b08f94
Omkar Linge <omkarlinge3026@gmail.com>
2026-03-02 17:07:55 +0530

    added Hello.lua

---
+++ Lua_Scripts/Hello.lua
@@ -0,0 +1,25 @@
+-- Hello.lua
+-- First Lua program
+
+-- print() displays output on terminal
+print("Hello Omkar 🚀")
+
+-- storing values in variables
+local name = "Omkar"
+local age = 21
+local isDeveloper = true
+
+-- printing stored values
+print("Name:", name)
+print("Age:", age)
+print("Developer:", isDeveloper)
+
+-- 🧠 What Happened
+--      print() displays text.
+--      Variables store data.
+--      Lua automatically detects data type.
+--      Values are printed using comma separation.
+-- ✅ Learning:
+--        Output
+--        Variables
+--        Basic syntax
